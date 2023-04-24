c7e693cf5dc3bb7b5e31dfcd988e24f5840428af
Ashish Khare <akstyles211@gmail.com>
2023-04-24 15:25:09 +0530

    Fixed Avatar size

--- next-app/components/Header.jsx
+++ next-app/components/Header.jsx
@@ -5,13 +5,13 @@ import Image from "next/image";
 export default function Header(props) {
 
     return (
-        <header className="flex flex-wrap items-center gap-3 mt-36 md:mt-24">
+        <header className="flex flex-wrap items-center gap-3 mt-24">
             <Image
                 src={props.icon}
                 width={520}
                 height={520}
                 alt="Rocklee"
-                className="rounded-full w-28 md:w-48 h-28 md:h-48 aspect-square border-white border-4 bg-green"
+                className="rounded-full w-48 h-48 aspect-square border-white border-4 bg-green"
             />
             <div className="flex flex-col order-last w-full sm:w-fit sm:order-none items-left gap-1 mt-auto mb-1">
                 <h1 className="text-4xl font-bold uppercase tracking-wider">{
